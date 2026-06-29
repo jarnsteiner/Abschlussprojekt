6c80068656b4cd2490dd625a1124b23d3865af25
Laurence Bichlbauer <lth.bichlbauer04@mci4me.at>
2026-06-29 17:06:32 +0200

    app_pages

--- main.py
+++ main.py
@@ -2,7 +2,35 @@ import streamlit as st
 from src.person import Person
 from src.ekg_data import EKGdata
 from src.read_data import load_person_data, get_name_to_id
-from pages import overview, sleep_analysis, account, ekg_analysis
+from app_pages import overview, sleep_analysis, account, ekg_analysis
+
+st.markdown("""
+<style>
+
+/* Titel */
+[data-testid="stSidebar"] h1 {
+    font-size: 2.5rem;
+}
+
+/* Radio-Buttons */
+[data-testid="stSidebar"] label {
+    font-size: 2rem;
+    font-weight: 700;
+}
+
+/* Abstand zwischen den Einträgen */
+[data-testid="stSidebar"] div[role="radiogroup"] > label {
+    padding-top: 30px;
+    padding-bottom: 30px;
+}
+
+/* Größere Kreise */
+[data-testid="stSidebar"] input[type="radio"] {
+    transform: scale(3);
+}
+
+</style>
+""", unsafe_allow_html=True)
 
 st.set_page_config(
     layout="wide",
@@ -10,7 +38,8 @@ st.set_page_config(
     page_icon= "🌙"
     )
 
-st.sidebar.title("🌙 Schlafanalyse")
+st.sidebar.title("🌙😴📊📈")
+st.sidebar.title("Schlafanalyse")
 
 page = st.sidebar.radio(
     "",
@@ -27,12 +56,12 @@ page = st.sidebar.radio(
 if page == "🏠 Übersicht":
     overview.show()
 
-elif page == "📊 Analyse":
+elif page == "💤 Schlafanalyse":
     sleep_analysis.show()
 
 elif page == "❤️ EKG-Datenanalyse":
     ekg_analysis.show()
 
-elif page == "👤 Account":
+elif page == "👤 Benutzer":
     account.show()
 
